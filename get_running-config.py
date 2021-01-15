7bed01f3ce199476a1454c824106f44925ee3815
JJameson-Neunzig <77113316+JJameson-Neunzig@users.noreply.github.com>
2021-01-14 18:48:06 -0600

    Update get_running-config.py

--- get_running-config.py
+++ get_running-config.py
@@ -1,12 +1,18 @@
+#+--------------------------------------------------------------------------+
+#|  THE FOLLOWING PYTHON FILE WAS CREATED BY JEAFFREY JAMESON BASED IN      |
+#|             DAVID BOMBAL'S JOB (MY MY GRATEFULNESS TO HIM)               |
+#+--------------------------------------------------------------------------+
+
 import getpass
 import sys
 import telnetlib
 
-HOST = "10.5.15.1"
-#USER = raw_input("Enter your telnet username: ")
-USER = "jeaffrey"
-#PASSWORD = getpass.getpass()
-PASSWORD = "passcisco"
+#ENTER BETWEEN THE "" THE IP OF YOUR CISCO SWITCH OR ROUTER
+HOST = " "
+#ENTER BETWEEN THE "" YOUR USER TO ACCESS TO YOUR CISCO SWITCH OR ROUTER
+USER = " "
+#ENTER BETWEEN THE "" THE IP OF YOUR CISCO SWITCH OR ROUTER
+PASSWORD = " "
 
 tn = telnetlib.Telnet(HOST)
 
@@ -16,23 +22,20 @@ if PASSWORD:
     tn.read_until("Password: ")
     tn.write(PASSWORD + "\n")
 
-#tn.write("show runn \n")
-#tn.write(" \n")
-#tn.write(" \n")
-#tn.write("int loop 0\n")
-#tn.write("ip address 1.1.1.1 255.255.255.255\n")
-#tn.write("int loop 1\n")
-#tn.write("ip address 2.2.2.2 255.255.255.255\n")
-#tn.write("router ospf 1\n")
-#tn.write("network 0.0.0.0 255.255.255.255 area 0\n")
-tn.write("show clock \n")
+#THIS PART IS TO EXPAND THE LENGTH OF THE LINES IN THE TELNET CONNECTION
+tn.write("config term \n")
+tn.write("line vty 0 4 \n")
+tn.write("login local \n")
+tn.write("length 512 \n")
+tn.write("transport input all \n")
+tn.write("end \n")
+
+#GET THE CONFIGURATION OF YOUR SWITCH OR ROUTER
+tn.write("show startup-config \n")
 tn.write("exit \n")
 
-#print tn.read_all()
-
-f = open("ESW11.txt", "a")
-
-#f = open("ESW11.txt", "w")
+#CREATE THE .TXT FILE, INSERT THE NAME THAT YOU WANT BETWEEN THE "" 
+f = open("ESW11.txt", "w")
 f.write(tn.read_all())
 f.close()
 
